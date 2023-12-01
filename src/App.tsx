f35691ce74aedce958d64e7231aeb8a0686a1d33
Lukematch <2522803379@qq.com>
2023-12-01 12:08:22 +0800

    first-construct

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className='App'>
+      component
+    </div>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// 样式初始化引入
+import "reset-css"
+// UI框架样式引入
+
+// 全局样式
+import "./assets/styles/global.scss"
+// 组件样式引入
+import App from './App'
+
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
